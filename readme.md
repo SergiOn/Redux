344c65fdb2309ab484e3096c2abdd378f61e739a
SergiOn <Onishhenko@gmail.com>
2017-03-05 23:16:53 +0200

    #3 subscription

--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
 <h1>Redux screencast</h1>
 
 <a target="_blank" href="https://www.youtube.com/watch?v=-m3evZuzXC8&index=1&list=PLqHlAwsJRxANFIgAf7BO8hNYdvipLERxQ">Link to screencast</a>
+
+<h3>#1 Reducer</h3>
+<h3>#2 Actions</h3>
+<h3>#3 Store</h3>
+<h3>#4 Subscription</h3>
---
+++ subscription.js
@@ -0,0 +1,41 @@
+function updateState(state, action) {
+    if (action.type === 'INCREMENT') {
+        return state + action.amount;
+    } else if (action.type === 'DECREMENT') {
+        return state - action.amount;
+    } else {
+        return state;
+    }
+}
+
+class Store {
+    constructor(updateState, state) {
+        this._updateState = updateState;
+        this._state = state;
+        this._callbacks = [];
+    }
+
+    get state() {
+        return this._state;
+    }
+
+    update(action) {
+        this._state = this._updateState(this._state, action);
+        this._callbacks.forEach(callback => callback());
+    }
+
+    subscribe(callback) {
+        this._callbacks.push(callback);
+    }
+}
+
+const store = new Store(updateState, 0);
+
+const incrementAction = { type: 'INCREMENT', amount: 5 };
+const decrementAction = { type: 'DECREMENT', amount: 3 };
+
+store.subscribe( function() {console.log('State changed', store.state)});
+
+store.update(incrementAction);
+store.update(decrementAction);
+store.update({});
