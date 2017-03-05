5d655ff9f3426f34510dbbe4075de9f234e8c787
SergiOn <Onishhenko@gmail.com>
2017-03-05 21:45:43 +0200

    #2 actions

---
+++ actions.js
@@ -0,0 +1,20 @@
+let state = 0;
+
+function updateState(state, action) {
+    if (action.type === 'INCREMENT') {
+        return state + action.amount;
+    } else if (action.type === 'DECREMENT') {
+        return state - action.amount;
+    } else {
+        return state;
+    }
+}
+
+state = updateState(state, { type: 'INCREMENT', amount: 5 });
+console.log(state);
+
+state = updateState(state, { type: 'DECREMENT', amount: 3 });
+console.log(state);
+
+state = updateState(state, {});
+console.log(state);
